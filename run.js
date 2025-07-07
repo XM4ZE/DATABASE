0fd8d7be287ccd37a4731a7c65203d145d2aca76
X <159207241+XM4ZE@users.noreply.github.com>
2025-07-07 20:18:38 +0700

    Update run.js

--- run.js
+++ run.js
@@ -2,14 +2,14 @@ const { spawn } = require('child_process');
 process.env.TZ = 'Asia/Jakarta';
 
 /**
- * Replace "I have no name!" with custom username (XMPANELS)
+ * Replace "I have no name!" with XMPANEL@user
  */
 function start(cmd) {
     try {
         const childProcess = spawn(cmd, ['-c', `
-            export USER="XMPANELS";
+            export USER="XMPANEL";
             export HOME="/home/container";
-            export PS1="XMPANELS@\\h:\\w\\$ ";
+            export PS1="XMPANEL@user:\\w\\$ ";
             bash --noprofile --norc
         `], {
             stdio: 'inherit'
