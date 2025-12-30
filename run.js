7e3b1b7f0e016eff2204e9b8ee62ea48b0d910d0
X <159207241+XM4ZE@users.noreply.github.com>
2025-12-31 01:46:36 +0700

    Update run.js

--- run.js
+++ run.js
@@ -1,14 +1,15 @@
-const { spawn } = require('child_process');
+import { spawn } from 'child_process';
+import { createRequire } from 'module';
 
+const require = createRequire(import.meta.url);
 process.env.TZ = 'Asia/Jakarta';
 
 /**
  * Start bash with colored prompt
  */
 function start(cmd) {
-    if (!cmd) cmd = 'bash';
-
     try {
+        // ANSI codes untuk warna
         const colorPrompt = '\\[\\033[1;36m\\]XMPanels@users\\[\\033[0m\\]:\\w\\$ ';
 
         const childProcess = spawn(cmd, ['-c', `
@@ -28,9 +29,10 @@ function start(cmd) {
     }
 }
 
-module.exports = start;
-module.exports.start = start; 
-
-if (require.main === module) {
-    start('bash');
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = { start };
 }
+
+export default start;
+
+start('bash');
